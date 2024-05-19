d37fa5ebdcaf96a264c88457aaea37d4015882d0
Aitor Pérez <aitorph7@hotmail.com>
2024-05-19 19:57:56 +0200

    First commit

---
+++ src/main/java/com/certidevs/springBootproject/BackendApiRestSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.certidevs.springBootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApiRestSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/certidevs/springBootproject/BackendApiRestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certidevs.springBootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiRestSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
